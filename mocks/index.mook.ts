7a24b86d666adea086a9beed9376786f68885da3
SampleUser <pedro.cunha@onfinity.com.br>
2022-08-06 19:50:50 -0300

    Create setup and mooks

---
+++ mocks/index.mook.ts
@@ -0,0 +1,33 @@
+export default {
+    initial: {
+        title: 'Quer vender mais, com ticket mais alto e ter mais lucratividade?',
+        subTitle: 'Estamos preparando muitas novidades sobre Marketing, Vendas e Growth que lhe ajudarão a montar uma verdadeira Máquina de Vendas!',
+        callToAction: 'Cadastre-se para ser avisado em primeira mão e para receber os links de nossas aulas gratuitas. ',
+        buttonText: 'Quero me cadastrar'
+    },
+    videoSection: {
+        title: 'Estratégias de Marketing e Vendas que geraram mais de R$ 250.000.000,00 em vendas nos últimos dois anos',
+        videoUrl: 'https://www.youtube.com/embed/QH2-TGUlwu4',
+        buttonText: 'Quero conhecer o método Sagaz'
+    },
+    formSection: {
+        callToAction: 'Se você se interessou por este método e quer crescer o seu negócio exponencialmente para receber nossas novidades em primeira mão. Além disso, você terá acesso gratuito a todas as aulas já gravadas pela equipe da Sagaz Growth Marketing.',
+        form: {
+            buttonText: 'Me cadastrar agora'
+        }
+    },
+    finalSection: {
+        photo: 'https://s3.amazonaws.com/sagaz-growth-marketing/assets/images/sagaz-growth-marketing-final-section.png',
+        description: 'Olá me chamo Sidimar Sagaz, sou Publicitário, Especialista em Marketing e Mestre em Administração de Empresas pela Universidade Federal do Rio Grande do Sul. Sou empreendedor desde os 20 anos de idade e passei os últimos anos me dedicando a aprender mais sobre processos de vendas, marketing digital e formação de times de alta performance. Depois de desenvolver e aplicar inúmeras estratégias de Marketing e Vendas que geraram mais de 250 milhões de reais em faturamento para meus clientes somente nos últimos dois anos, decidi sair dos bastidores e revelar todo o meu método para estruturação de verdadeiras Máquinas de Vendas. Através da Sagaz Growth Marketing e de seu ecossistema potencializarei o poder de crescimento de profissionais e empresas. Me siga nas redes sociais para ficar por dentro de tudo! Até breve! #BoraCrescerJuntos',
+        instagram: 'https://www.instagram.com/sagazgrowthmarketing/',
+        youtube: 'https://www.youtube.com/channel/UC-9-kyTW8ZkZNDHQJ6FgpwQ',
+        facebook: 'https://www.facebook.com/sagazgrowthmarketing/',
+        linkedin: 'https://www.linkedin.com/company/sagaz-growth-marketing/'
+    },
+    footer: {
+        text: 'Copyright © 2022 | SGZ Marketing e Vendas -CNPJ: 46.351.591/0001-42'
+    },
+    whatsappButton: {
+        phone: '+55 (47) 9 9999-9999'
+    }
+}
