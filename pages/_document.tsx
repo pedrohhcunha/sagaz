dc0dc9975d69aa92982f13c6b5d7d02b33dafb20
SampleUser <pedro.cunha@onfinity.com.br>
2022-09-12 17:11:44 -0300

    Insert facebook pixel

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,6 +8,23 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.googleapis.com"></link>
         <link rel="preconnect" href="https://fonts.gstatic.com"></link>
         <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Montserrat:wght@300;400;500;600;700;800&display=swap" rel="stylesheet"></link>
+        <script dangerouslySetInnerHTML={{
+          __html: `
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window,document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1113491476241637'); 
+            fbq('track', 'PageView');
+          `
+        }}></script>
+        <noscript dangerouslySetInnerHTML={{
+          __html: `<img height="1" width="1" src="https://www.facebook.com/tr?id=1113491476241637&ev=PageView&noscript=1"/>`
+        }}></noscript>
       </Head>
       <body>
         <Main />
